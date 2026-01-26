67abb05347cd5458acafc3ba8a6d96c3e1fb361e
Anton Alexeev <gonza_ant@mail.ru>
2026-01-26 14:58:28 +0300

    Prepare to HW0

---
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -0,0 +1,17 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class UserMealWithExcess {
+    private final LocalDateTime dateTime;
+    private final String description;
+    private final int calories;
+    private final boolean excess;
+
+    public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+}
