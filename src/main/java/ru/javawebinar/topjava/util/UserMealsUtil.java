a721fa3ea9d94a1c3ba77a9999f69dea9182945d
Anton Alexeev <gonza_ant@mail.ru>
2026-01-28 07:26:19 +0300

    completed HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -26,17 +26,17 @@ public class UserMealsUtil {
             new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
             new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
-
+        System.out.println("Результат работы filteredByCyclesOption1:");
         List<UserMealWithExcess> mealsTo1 = filteredByCyclesOption1(meals,
                 LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo1.forEach(System.out::println);
-
+        System.out.println("Результат работы filteredByCyclesOption2:");
         List<UserMealWithExcess> mealsTo2 = filteredByCyclesOption2(meals,
                 LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo2.forEach(System.out::println);
-
+        System.out.println("Результат работы filteredByStreamsOption1:");
         System.out.println(filteredByStreamsOption1(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
-
+        System.out.println("Результат работы filteredByStreamsOption1:");
         System.out.println(filteredByStreamsOption2(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
