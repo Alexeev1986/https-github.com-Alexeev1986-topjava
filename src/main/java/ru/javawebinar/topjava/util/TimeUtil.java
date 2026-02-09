10aa43b0e7b82123fc22a954a7a07b4b19dc5fe4
Anton Alexeev <gonza_ant@mail.ru>
2026-02-09 13:47:19 +0300

    upgrade logic in method isBetweenHalfOpen from TimeUtil

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -4,12 +4,6 @@ import java.time.LocalTime;
 
 public class TimeUtil {
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        if (startTime != null && lt.isBefore(startTime)) {
-            return false;
-        }
-        if (endTime != null && !lt.isBefore(endTime)) {
-            return false;
-        }
-        return true;
+        return (startTime == null || !lt.isBefore(startTime)) && (endTime == null || lt.isBefore(endTime));
     }
 }
