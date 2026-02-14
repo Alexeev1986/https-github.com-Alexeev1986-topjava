a5805a65e02a2b20e3007ca404161d5655b36e61
Anton Alexeev <gonza_ant@mail.ru>
2026-02-14 07:24:48 +0300

    fix verification of all operations MealRestController

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -16,24 +16,28 @@ public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management  (ARM)
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-
+            System.out.println("\nBean definition names: ");
+            String[] binsArr = appCtx.getBeanDefinitionNames();
+            for (int i = 0; i < binsArr.length; i++) {
+                System.out.println(i + ": " + binsArr[i]);
+            }
+            System.out.println("\nCreate user:");
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
 
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
             try {
                 Collection<Meal> meals = mealRestController.getAll();
-                System.out.println("getAll():");
+                System.out.println("\ngetAll():");
                 meals.forEach(System.out::println);
-                System.out.println("get(1):");
+                System.out.println("\nget(1):");
                 System.out.println(mealRestController.get(1));
                 Meal meal = new Meal(1, null, LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Ужин", 557);
-                System.out.println("save:");
+                System.out.println("\nsave:");
                 System.out.println(mealRestController.create(meal));
-                System.out.println("delete:");
+                System.out.println("\ndelete:");
                 mealRestController.delete(1);
-                System.out.println("update:");
+                System.out.println("\nupdate:");
                 Meal upadateMeal = new Meal(1, 8, LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "Ужин", 777);
                 mealRestController.update(upadateMeal);
             } catch (Exception e) {
