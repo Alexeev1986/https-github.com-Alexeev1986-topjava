207af99bc6929bd1efa653f3bca3c234893fa517
Anton Alexeev <gonza_ant@mail.ru>
2026-02-23 20:43:29 +0300

    Create printBeans method in SpringMain

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -10,6 +10,7 @@ import ru.javawebinar.topjava.web.user.AdminRestController;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,27 +20,60 @@ public class SpringMain {
         //System.setProperty("spring.profiles.active", "inmemory");
         System.setProperty("spring.profiles.active", "jdbc");
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml")) {
-            System.out.println("=== Active profiles: " +
-                    Arrays.toString(appCtx.getEnvironment().getActiveProfiles()));
-            System.out.println("=== MealRepository beans: " +
-                    Arrays.toString(appCtx.getBeanNamesForType(
-                            ru.javawebinar.topjava.repository.MealRepository.class)));
 
-            System.out.println("Bean definition names: ");
-            Arrays.stream(appCtx.getBeanDefinitionNames()).forEach(System.out::println);
-            System.out.println("End beans!");
+
+           printBeans(appCtx);
+
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+
             //adminUserController.create(new User(null, "userName", "email1@mail.ru", "password", Role.ADMIN));
-            System.out.println(SecurityUtil.authUserId());
+
+            System.out.println("Authorization user: " + SecurityUtil.authUserId());
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
+
             List<MealTo> filteredMealsWithExcess =
                     mealController.getBetween(
                             LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
                             LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
             filteredMealsWithExcess.forEach(System.out::println);
             System.out.println();
+
             mealController.getBetween(null, null, null, null).forEach(System.out::println);
         }
+    }
+
+    private static void printBeans(ConfigurableApplicationContext appCtx) {
+        System.out.println("=== Active profiles: " +
+                Arrays.toString(appCtx.getEnvironment().getActiveProfiles()));
+
+        System.out.println("=== MealRepository beans: " +
+                Arrays.toString(appCtx.getBeanNamesForType(
+                        ru.javawebinar.topjava.repository.MealRepository.class)));
+
+        String[] beanNames = appCtx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        List<String> beans = new ArrayList<>();
+        List<String> infrastructureBeans = new ArrayList<>();
+        for (String name : beanNames) {
+            try {
+                Object bean = appCtx.getBean(name);
+                String beanClassName = bean.getClass().getName();
+                String result = "Bean : " + name + ", Class: " + bean.getClass().getName();
+                if (beanClassName.startsWith("ru.javawebinar.topjava")) {
+                    beans.add(result);
+                } else {
+                    infrastructureBeans.add(result);
+                }
+            } catch (Exception e) {
+                System.out.println(name + "[Definition only, not instantiated or complex]");
+            }
+        }
+        System.out.println("YOUR APPLICATION BEANS: ");
+        beans.forEach(System.out::println);
+        System.out.println("END YOUR BEANS");
+        System.out.println("INFRASTRUCTURE BEANS: ");
+        infrastructureBeans.forEach(System.out::println);
+        System.out.println("END INFRASTRUCTURE BEANS");
     }
 }
