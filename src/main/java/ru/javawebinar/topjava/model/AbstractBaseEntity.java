c7cfda4d442beab3e2eb39ee973a392d7d681500
Anton Alexeev <gonza_ant@mail.ru>
2026-03-05 11:36:35 +0300

    5_7_spring_data_jpa

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.model;
 
+import org.springframework.data.domain.Persistable;
 import org.springframework.util.Assert;
 
 import javax.persistence.*;
@@ -10,7 +11,7 @@ import static org.hibernate.proxy.HibernateProxyHelper.getClassWithoutInitializi
 
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
-public abstract class AbstractBaseEntity {
+public abstract class AbstractBaseEntity implements Persistable<Integer> {
     public static final int START_SEQ = 100000;
 
     @Id
@@ -31,6 +32,7 @@ public abstract class AbstractBaseEntity {
         this.id = id;
     }
 
+    @Override
     public Integer getId() {
         return id;
     }
@@ -41,6 +43,7 @@ public abstract class AbstractBaseEntity {
         return id;
     }
 
+    @Override
     public boolean isNew() {
         return getId() == null;
     }
