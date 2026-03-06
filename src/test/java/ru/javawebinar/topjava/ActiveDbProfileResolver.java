2053037fe5ad4790260f1b751121cbfe005ca5a1
Anton Alexeev <gonza_ant@mail.ru>
2026-03-06 15:46:40 +0300

    Create MealServiceDataJpaTest, MealServiceJpaTest, MealServiceJdbcTest. Moved Profiles from test folder.

--- src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
+++ src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
@@ -1,18 +1,36 @@
 package ru.javawebinar.topjava;
 
+import org.slf4j.Logger;
 import org.springframework.lang.NonNull;
 import org.springframework.test.context.support.DefaultActiveProfilesResolver;
 
 import java.util.Arrays;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 //http://stackoverflow.com/questions/23871255/spring-profiles-simple-example-of-activeprofilesresolver
 public class ActiveDbProfileResolver extends DefaultActiveProfilesResolver {
+    private static final Logger log = getLogger(ActiveDbProfileResolver.class);
+
     @Override
     public @NonNull String[] resolve(@NonNull Class<?> aClass) {
         // https://stackoverflow.com/a/52438829/548473
         String[] activeProfiles = super.resolve(aClass);
+        String dbProfile = Profiles.getActiveDbProfile();
         String[] activeProfilesWithDb = Arrays.copyOf(activeProfiles, activeProfiles.length + 1);
         activeProfilesWithDb[activeProfiles.length] = Profiles.getActiveDbProfile();
+
+        log.info("\n========================================\n" +
+                        "ActiveDbProfileResolver for: {}\n" +
+                        "  • Explicit profiles:  {}\n" +
+                        "  • Detected DB profile: {}\n" +
+                        "  • Final active profiles: {}\n" +
+                        "========================================",
+                aClass.getSimpleName(),
+                Arrays.toString(activeProfiles),
+                dbProfile,
+                Arrays.toString(activeProfilesWithDb));
+
         return activeProfilesWithDb;
     }
 }
