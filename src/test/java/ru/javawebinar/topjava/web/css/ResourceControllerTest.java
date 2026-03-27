5fb3eb09c65e91b3e0ca86ffabac05898d4d7799
Anton Alexeev <gonza_ant@mail.ru>
2026-03-27 17:36:28 +0300

    Create RootControllerTest.getMeals, RootControllerTest.getMealsWithTestMatcher and ResourceControllerTest.

---
+++ src/test/java/ru/javawebinar/topjava/web/css/ResourceControllerTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web.css;
+
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+    @Test
+    void getStyleCss() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("text/css"))
+                .andExpect(content().encoding("UTF-8"));
+
+    }
+}
