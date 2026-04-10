35050629ca0947ed3a04c86fe9637a5b975efb2a
Anton Alexeev <gonza_ant@mail.ru>
2026-04-10 21:16:42 +0300

    Converted meals.jsp to work with Ajax. Made the table row style based on excess, and the time is displayed without the T. Added i18n.

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -19,34 +19,30 @@ function clearFilter() {
 
 $(function () {
     makeEditable(
-        $("#datatable").DataTable({
+        $("#datatable").DataTable({"ajax": { "url": mealAjaxUrl, "dataSrc": "" },
             "paging": false,
             "info": true,
             "columns": [
-                {
-                    "data": "dateTime"
+                { "data": "dateTime", "render": function (date, type, row) {
+                        if (type === "display") {
+                            return date.replace('T', ' ').substring(0, 16);
+                        }
+                        return date;
+                  }
                 },
-                {
-                    "data": "description"
-                },
-                {
-                    "data": "calories"
-                },
-                {
-                    "defaultContent": "Edit",
-                    "orderable": false
-                },
-                {
-                    "defaultContent": "Delete",
-                    "orderable": false
-                }
+                { "data": "description" },
+                { "data": "calories" },
+                { "orderable": false, "defaultContent": "", "render": renderEditBtn },
+                { "orderable": false, "defaultContent": "", "render": renderDeleteBtn }
             ],
-            "order": [
-                [
-                    0,
-                    "desc"
-                ]
-            ]
+            "order": [[0, "desc"]],
+            "createdRow": function (row, data, dataIndex) {
+                if (data.excess) {
+                    $(row).attr("data-meal-excess", true);
+                } else {
+                    $(row).attr("data-meal-excess", false);
+                }
+            }
         })
     );
 });
--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -27,63 +27,37 @@ function enable(chkbox, id) {
 $(function () {
     makeEditable(
         $("#datatable").DataTable({
-            "ajax": {
-                "url": userAjaxUrl,
-                "dataSrc": ""
-            },
+            "ajax": { "url": userAjaxUrl, "dataSrc": "" },
             "paging": false,
             "info": true,
             "columns": [
-                {
-                    "data": "name"
-                },
-                {
-                    "data": "email",
-                    "render": function (data, type, row) {
-                        if (type === "display") {
-                            return `<a href="mailto:${data}">${data}</a>`;
-                        }
-                        return data;
+                { "data": "name" },
+                { "data": "email", "render": function (data, type, row) {
+                    if (type === "display") {
+                        return `<a href="mailto:${data}">${data}</a>`;
                     }
+                    return data;
+                 }
                 },
-                {
-                    "data": "roles"
-                },
-                {
-                    "data": "enabled",
-                    "render": function (data, type, row) {
-                        if (type === "display") {
-                            return `<input type='checkbox' ${data ? 'checked' : ''} onclick='enable($(this),${row.id});'/>`;
-                        }
-                        return data;
+                { "data": "roles" },
+                { "data": "enabled", "render": function (data, type, row) {
+                    if (type === "display") {
+                        return `<input type='checkbox' ${data ? 'checked' : ''} onclick='enable($(this),${row.id});'/>`;
                     }
+                    return data;
+                 }
                 },
-                {
-                    "data": "registered",
-                    "render": function (date, type, row) {
-                        if (type === "display") {
-                            return date.substring(0, 10);
-                        }
-                        return date;
+                { "data": "registered", "render": function (date, type, row) {
+                    if (type === "display") {
+                        return date.substring(0, 10);
                     }
+                    return date;
+                 }
                 },
-                {
-                    "orderable": false,
-                    "defaultContent": "",
-                    "render": renderEditBtn
-                },
-                {
-                    "orderable": false,
-                    "defaultContent": "",
-                    "render": renderDeleteBtn
-                }
+                { "orderable": false, "defaultContent": "", "render": renderEditBtn },
+                { "orderable": false, "defaultContent": "", "render": renderDeleteBtn }
             ],
-            "order": [
-                [
-                    0,
-                    "asc"
-                ]
-            ],
+            "order": [[0, "asc"]],
             "createdRow": function (row, data, dataIndex) {
                 if (!data.enabled) {
                     $(row).attr("data-user-enabled", false);
