c16fc8d71274482d6e11d31e3478d73e7a2a3b08
Anton Alexeev <gonza_ant@mail.ru>
2026-04-12 17:24:52 +0300

    Corrected method update in MealService.

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -37,7 +37,7 @@ public class MealService {
     }
 
     public void update(Meal meal, int userId) {
-        get(meal.id(), userId);
+        Assert.notNull(meal, "meal must not be null");
         checkNotFound(repository.save(meal, userId), meal.id());
     }
 
