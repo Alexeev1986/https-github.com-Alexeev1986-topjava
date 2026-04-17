0440c1687fc799cd779c96339e2e2806b2932826
Anton Alexeev <gonza_ant@mail.ru>
2026-04-17 17:40:52 +0300

    Prepare to HW10

--- src/main/java/ru/javawebinar/topjava/web/interceptor/ModelInterceptor.java
+++ src/main/java/ru/javawebinar/topjava/web/interceptor/ModelInterceptor.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
  * This interceptor adds userTo to the model of every requests
  */
 public class ModelInterceptor implements HandlerInterceptor {
-
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         if (modelAndView != null && !modelAndView.isEmpty()) {
